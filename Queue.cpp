d3e0d44ab6344bfac4a6ba768b61ca8f3178e2e9
ethanxtreme <ethan4083@gmail.com>
2022-12-04 22:10:06 -0800

    Worked in implementation, Dequeue not working.

--- Queue.cpp
+++ Queue.cpp
@@ -4,30 +4,51 @@
 template<class T>
 Queue<T>::Queue(int max)
 {
-
+    head = nullptr;
+    tail = nullptr;
+    this->max = max;
+    numElems = 0;
 }
 
 template<class T>
 Queue<T>::Queue()
 {
-
+    head = nullptr;
+    tail = nullptr;
 }
 
 template<class T>
 Queue<T>::~Queue()
 {
-
+    Node* currNode = head;
+    while(currNode != nullptr){
+        Node* temp = currNode;
+        currNode = currNode->next;
+        delete temp;
+    }
+    head = nullptr;
+    tail = nullptr;
 }
 
 template<class T>
 void Queue<T>::MakeEmpty()
 {
-
+    Node* currNode = head;
+    while(currNode != nullptr){
+        Node* temp = currNode;
+        currNode = currNode->next;
+        delete temp;
+    }
+    head = nullptr;
+    tail = nullptr;
 }
 
 template<class T>
 bool Queue<T>::IsEmpty() const
 {
+    if(head == nullptr){
+        return true;
+    }
     return false;
 }
 
@@ -35,17 +56,47 @@ template<class T>
 bool Queue<T>::IsFull() const
 // Returns true if the queue is full; false otherwise.
 {
+    if(numElems>=max){
+        return true;
+    }
     return false;
 }
 
 template<class T>
 void Queue<T>::Enqueue(T newItem)
 {
+    // Function: Adds newItem to the rear of the queue.
+    // Post: If (queue is full) FullQueue exception is thrown
+    //       else newItem is at rear of queue.
+    //Beg, empty, end, mid
+    if(IsFull()){
+        throw "Full Queue";
+    }
+    //create and initialize newnode
+    Node* newNode = new Node;
+    newNode->value = newItem;
+    newNode->next = nullptr;
+    numElems++;
 
+    if(head == nullptr){//if there is no queue
+        head == newNode;
+        tail == newNode;
+        return;
+    } else{
+        tail->next = newNode;
+        tail = newNode;
+    }
 }
 
 template<class T>
 T Queue<T>::Dequeue()
 {
-    return T();
+    if(IsEmpty()){
+        throw "Empty Queue";
+    }
+    T returnVal = head->value;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return returnVal;
 }
